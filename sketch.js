eab50bdb86e98453ddfcbb144e615698534dd2a8
c4iden <86789530+c4iden@users.noreply.github.com>
2021-07-07 10:09:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+//             0  1  2  3  4  5  6  7  8  9
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
